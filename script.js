17d30253f5d026879f74107a29905dd41d1ddbe1
Thaqif Azfar <thaqifazfar15@gmail.com>
2025-10-25 21:14:35 +0100

    draw simple car window

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d'); // 2D rendering context
 
-ctx.fillStyle = 'blue'; // Set fill color
-ctx.fillRect(0, -10, 150, 100); // Draw a filled rectangle (x, y, width, height)
+ctx.fillStyle = 'black'; // Set fill color
+ctx.fillRect(0, 0, 600, 400); // Draw a filled rectangle (x, y, width, height)
 
+// ctx.beginPath();
+// ctx.arc(200, 150, 50, 0, Math.PI * 2); // x, y, radius, startAngle, endAngle
+// ctx.fillStyle = 'red';
+// ctx.fill();
+// ctx.closePath();
+
+
+// begin drawing windows seat
 ctx.beginPath();
-ctx.arc(200, 150, 50, 0, Math.PI * 2); // x, y, radius, startAngle, endAngle
-ctx.fillStyle = 'red';
-ctx.fill();
-ctx.closePath();
+ctx.strokeStyle = "white"
+// Set start-point
+ctx.moveTo(50, 350); // (x , y)
+
+// Set sub-points
+ctx.lineTo(550, 350);
+ctx.lineTo(550, 50);
+ctx.lineTo(550, 50);
+ctx.quadraticCurveTo(150, 40, 50, 250);
+ctx.lineTo(50, 350); 
+
+
+// Stroke it (do the drawing)
+ctx.stroke();
